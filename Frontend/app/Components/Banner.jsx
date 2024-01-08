6d742eb051db4dd2d66b4616817a3d97d9122630
DS <dsrathorebsw1234@gmail.com>
2024-01-08 21:06:10 +0530

    Created Banner of Friend route

--- Frontend/app/Components/Banner.jsx
+++ Frontend/app/Components/Banner.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Image from "next/image";
 import BannerImg from "../Assets/Banner_Image.jpg";
+import ProfilePic from "../Assets/profilePic.jpg";
+import { BsFilePost } from 'react-icons/bs';
+import { BiGroup, BiPhotoAlbum } from 'react-icons/bi';
 
 const Banner = () => {
     return (
         <>
-            <div className='flex flex-col justify-center items-evenly'>
-                <Image className='w-full' alt='Banner_Image' src={BannerImg} height={200} width={400} />
-                <div className=''>
-                    Hello
+            <div className=''>
+                <div className='flex flex-col justify-center items-evenly bg-white rounded-xl shadow-md shadow-gray-200'>
+                    <Image className='w-full h-[30vh] object-fill rounded-ss-xl rounded-se-xl' alt='Banner_Image' src={BannerImg} height={200} width={400} />
+                    <div className='relative'>
+                        <Image className='absolute -top-20 left-6 rounded-full h-40 w-40 object-cover' src={ProfilePic} alt="ProfilePic" width={200} height={200} />
+                        <div className='ml-[12rem] mt-2'>
+                            <h1 className='text-3xl font-bold'>John Doe</h1>
+                            <p className='text-gray-300 font-light'>Stockholm, Sweden</p>
+                        </div>
+                    </div>
+                    <ul className='flex w-full mt-12 py-3 justify-around items-center'>
+                        <li className='flex gap-2 justify-center items-center'><BsFilePost />Post</li>
+                        <li className='flex gap-2 justify-center items-center'><span className='text-xl'>ℹ</span> About</li>
+                        <li className='flex gap-2 justify-center items-center'><BiGroup />Friends</li>
+                        <li className='flex gap-2 justify-center items-center'><BiPhotoAlbum />Photos</li>
+                    </ul>
+                </div>
+                <div className='bg-white h-full w-full rounded-xl    mt-5 p-5'>
+                    <h1 className='text-2xl uppercase'>Friends</h1>
                 </div>
             </div>
         </>
